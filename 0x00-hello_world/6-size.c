2615e66434bfa2fe9442285f1a06c887f626abfd
Alabeni Omosheye David <omosheye007@gmail.com>
2022-09-08 15:23:29 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char is 1 byte\n");
+	printf("size of int is 4 bytes\n");
+	printf("size of long int is 4 bytes\n");
+	printf("size of long long int is 8 bytes\n");
+	printf("size of float is 4 bytes\n");
+
+	return (0);
+}
