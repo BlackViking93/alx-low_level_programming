dbfd2945f8cf04396b57a263c322e9b17ea0c629
Alabeni Omosheye David <omosheye007@gmail.com>
2022-09-13 16:29:09 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,8 +4,8 @@
  *
  * @c: alpabet to check for
  *
- * Return: 1 if alphabet is lower or uppercase, otherwise 0*/
-
+ * Return: 1 if c is a letter, 0 otherwise
+ */
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
