5c6beb616916becd0566f989acdbe1a67171a113
Alabeni Omosheye David <omosheye007@gmail.com>
2022-09-13 14:42:54 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Description: contains all functions prototype used in this project
+ */
 
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,4 +20,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif
+#endif /* MAIN_H */
