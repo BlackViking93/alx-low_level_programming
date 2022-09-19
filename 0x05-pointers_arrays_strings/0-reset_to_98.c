5ea139bf6ee22f9ae418b62af67ec36f5012d5a4
Alabeni Omosheye David <omosheye007@gmail.com>
2022-09-19 06:38:37 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,18 +2,11 @@
 #include "main.h"
 
 /*
- * reset_to_98 - updates value of a variable
+ * Description: - updates value of a variable
  * Return: Always 0 (success)
  */
 
 void reset_to_98(int *n)
 {
-
-	int p;
-
-	n  = &p;
-
 	*n = 98;
-
-	return;
 }
